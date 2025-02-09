a6353f757be8d2d926d1c1b6bf73bd517beaf35d
BasicTree <andrewgra3@gmail.com>
2025-02-09 18:43:32 -0500

    Card adaptation

--- src/my-card.js
+++ src/my-card.js
@@ -14,7 +14,12 @@ export class MyCard extends LitElement {
 
   constructor() {
     super();
-    this.title = "My card";
+    this.title = "The card title";
+    this.imagelink = "https://safesendsoftware.com/wp-content/uploads/2016/06/Human-Error.jpg";
+    this.description = "A description of the card";
+    this.linklocale = "#";
+    this.linkdetails = "More details";
+    this.backgroundcolor = css `green`;
   }
 
   static get styles() {
@@ -22,18 +27,71 @@ export class MyCard extends LitElement {
       :host {
         display: block;
       }
-    `;
+      .card {
+        margin: 5px;
+        background-color: red;
+        display: inline-flex;
+        box-shadow: 5px 5px 5px ;
+        transition: .6s all ease-in-out;
+        border-radius: 15px;
+      }
+      .card2 {
+      background-color: #802000;
+      display: inline-flex;
+      }
+      .card-img {
+      margin: 15px;
+      height: 150px;
+      display: block;
+      }
+      .descriptbox {
+      margin-right: 20px;
+      margin-left: 20px;
+      margin-bottom: 20px;
+      padding: 10px;
+      width: 250px;
+      background-color: white;
+      border-radius: 10px;
+      box-shadow: 2px 2px 2px inset; 
+      }
+      .linkdetails{
+      margin-left: 90px;
+      }
+      `;
   }
 
   render() {
-    return html`<div>${this.title}</div>`;
+    return html`
+    <div class="card">
+        <div class="cardcontent">
+          <img class="card-img" src=${this.imagelink}>
+          <div class="descriptbox">
+            <h3 class="header">${this.title}</h3>
+            <p>  ${this.description}
+            </p>
+            <a class="linkdetails" href=${this.linklocale}>
+              ${this.linkdetails}
+            </a>
+          </div>
+        </div>
+    </div>
+    
+    `;
   }
 
   static get properties() {
     return {
       title: { type: String },
+      imagelink: { type: String },
+      description: { type: String },
+      linklocale: { type: String },
+      linkdetails: { type: String },
+      backgroundcolor: { type: String }
     };
   }
 }
 
 globalThis.customElements.define(MyCard.tag, MyCard);
+
+
+// Making buttons work is hard. I could not do dat yet.
